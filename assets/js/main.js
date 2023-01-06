7d03f1871f368b6f0a00c690dbb9a6e374522893
ywalakamar <barxwells@gmail.com>
2023-01-06 19:02:06 +0300

    feat(portfolio): Add Portfolio
    - Add portfolio component
    - Add `Swiper library` for swiping and pagination

--- assets/js/main.js
+++ assets/js/main.js
@@ -95,3 +95,17 @@ modalCloses.forEach((modalClose) => {
     });
   });
 });
+
+/*==============Portfolio Swiper============== */
+var swiper = new Swiper(".portfolio-container", {
+  cssMode: true,
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
