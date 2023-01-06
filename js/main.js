647b891a965333b20d236731302892478ed25a2c
ywalakamar <barxwells@gmail.com>
2023-01-06 14:39:18 +0300

    Add Services Component

--- js/main.js
+++ js/main.js
@@ -72,3 +72,26 @@ tabs.forEach((tab) => {
     tab.classList.add("tab-active");
   });
 });
+
+/*==============Service Modal============== */
+const modalViews = document.querySelectorAll(".services-modal"),
+  modalBtns = document.querySelectorAll(".services-button"),
+  modalCloses = document.querySelectorAll(".services-modal-close");
+
+let modal = function (modalClick) {
+  modalViews[modalClick].classList.add("active-modal");
+};
+
+modalBtns.forEach((modalBtn, i) => {
+  modalBtn.addEventListener("click", () => {
+    modal(i);
+  });
+});
+
+modalCloses.forEach((modalClose) => {
+  modalClose.addEventListener("click", () => {
+    modalViews.forEach((modalView) => {
+      modalView.classList.remove("active-modal");
+    });
+  });
+});
