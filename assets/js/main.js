e7899935b71402306f6da32b8d6b7d84d2f2db5c
ywalakamar <barxwells@gmail.com>
2023-01-07 17:53:08 +0300

    feat(scroll): Add Active Links and Section Scroll

--- assets/js/main.js
+++ assets/js/main.js
@@ -130,3 +130,28 @@ var swiperTestimonial = new Swiper(".testimonial-container ", {
     },
   },
 });
+
+/*==============Section Scroll Active Link============== */
+const sections = document.querySelectorAll("section[id]");
+
+function scrollActive() {
+  const scrollY = window.pageYOffset;
+
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight;
+    const sectionTop = current.offsetTop - 50;
+    const sectionId = current.getAttribute("id");
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document
+        .querySelector(".nav-menu a[href*=" + sectionId + "]")
+        .classList.add("active-link");
+    } else {
+      document
+        .querySelector(".nav-menu a[href*=" + sectionId + "]")
+        .classList.remove("active-link");
+    }
+  });
+}
+
+window.addEventListener("scroll", scrollActive);
